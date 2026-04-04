66ed38f7a16685cfb73a47f0fbdb3069b744daa2
Robert <robert.44azz@gmail.com>
2026-04-04 20:54:41 -0300

    First commit

---
+++ Aula 1/funcionamento_de_um_programa/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main(){ 
+  //caracteres
+  char nome_da_variavel;
+  //numeros inteiros
+  int nome_da_variavel;
+  short int nome_da_variavel;
+  long int nome_da_variavel;
+  //numeros reais
+  float nome_da_variavel;
+  double nome_da_variavel;
+  long double nome_da_variavel;
+}
